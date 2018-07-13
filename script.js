aaf67bac3088acfd3e12ee4110fc25f01d7874e6
Minyao Tan <mtan3@uw.edu>
2018-07-12 19:56:04 -0700

    by Minyao

--- script.js
+++ script.js
@@ -16,6 +16,8 @@
 
 
 let clickable = document.querySelector("img.clickable")
+let tab = document.getElementsByTagName("title")[0]
+let h2Text = document.getElementsByTagName("h2")[0]
 
 clickable.addEventListener("click", clickCounter)
 
@@ -24,13 +26,21 @@ let counter = 0
 console.log("counter: ", counter)
 
 function clickCounter(){
-counter++
-console.log("counter", counter)
+	counter++
+	console.log("counter", counter)
 
+	tab.innerHTML = counter + " click(s)!"
 
+	let container = document.querySelector("p#clickCount")
 
-let container = document.querySelector("p#clickCount")
-container.innerHTML = counter
-
-
+	if (counter == 10 || counter == 20) {
+		container.innerHTML = ""
+		h2Text.setAttribute("style", "color:red")
+		h2Text.innerHTML = counter
+		h2Text.style.transition = "all 2s"
+		h2Text.style.transform = "rotate(360deg)"
+	} else {
+		h2Text.innerHTML = ""
+		container.innerHTML = counter
+	}
 }
