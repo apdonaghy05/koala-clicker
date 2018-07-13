e258248357ce808c188d605f34497a60905a9088
Andrew <apd05@uw.edu>
2018-07-12 19:01:10 -0700

    Made edits

---
+++ script.js
@@ -0,0 +1,35 @@
+// click handler
+
+// track total clicks
+
+// output click count to container in dom
+
+
+// get element that we want
+
+// addEventListener syntax
+
+
+
+// increment counter variable "outside of click hadler"
+
+
+
+let clickable = document.querySelector("img.clickable")
+
+clickable.addEventListener("click", clickCounter)
+
+
+let counter = 0
+console.log("counter: ", counter)
+
+function clickCounter(){
+counter++
+console.log("counter", counter)
+
+
+let container = document.querySelector("p#ClickCount")
+container.innerHTML = counter
+
+
+}
