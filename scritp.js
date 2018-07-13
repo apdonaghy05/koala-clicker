bde59300e39306a14fef6fca166e8d47d958832b
Andrew <apd05@uw.edu>
2018-07-12 18:30:28 -0700

    created repo

---
+++ scritp.js
@@ -0,0 +1,14 @@
+// click handler
+
+// track total clicks
+
+// output click count to container in dom
+
+
+// get element that we want
+
+// addEventListener syntax
+
+
+
+// increment counter variable "outside of click hadler"
